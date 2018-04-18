ad8421a479d871a0d62733a0965d159973ff3146
unknown <758507292@qq.com>
2018-04-18 16:43:15 +0800

    添加gitignore

--- dist/js/index.js
+++
@@ -1,1 +0,0 @@
-angular.module("app",["ui.router"]),angular.module("app").config(["$stateProvider","$urlRouterProvider",function(e,l){e.state("main",{url:"/main",templateUrl:"view/main.html"}).state("login",{url:"/login",templateUrl:"view/login.html"}),l.otherwise("login")}]);
