226a0851e1e90933a84365e35fb5de3e99aeaeb1
“zhoujian <l>
2018-04-19 08:37:31 +0800

    登录页面

--- src/script/controller/loginCtrl.js
+++ src/script/controller/loginCtrl.js
@@ -1,7 +1,17 @@
-angular.module('app').controller('loginCtrl', ['$scope',function ($scope) {
+angular.module('app').controller('loginCtrl', ['$scope', '$state',function ($scope, $state) {
     console.log('login')
     $scope.formData = {
         name: '',
         password: ''
+    };
+    $scope.handleLogin = function () {
+        console.dir($scope.formData)
+        $state.go('main')
+    }
+    $scope.handleReset = function () {
+        $scope.formData = {
+            name: '',
+            password: ''
+        }
     }
 }])
